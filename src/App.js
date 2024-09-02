32f4a0dbea335ed227b39bf61fb6ac892f4dd455
Christina <yickind@hotmail.co.uk>
2024-09-02 14:56:43 +0100

    Initial commit to branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Tabs from './components/Tabs/Tabs';
+import WordList from './components/WordList/WordList';
+import Search from './components/Search/Search';
+
+const levels = [1, 2, 3, 4, 5]
 
 function App() {
+  const [currentLevel, setCurrentLevel] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetchWords();
+  }, [currentLevel]);
+
+  useEffect(() => {
+    filterWords();
+  }, [searchQuery]);
+
+  const fetchWords = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/v1/word?hsk=${currentLevel}`);
+      setWords(response.data);
+      console.log(words);
+      
+    } catch (error) {
+      console.error("Error fetching words:", error);
+    }
+  };
+
+  const filterWords = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/v1/word?english=${searchQuery}`);
+      setWords(response.data);
+      console.log(words);
+      
+    } catch (error) {
+      console.error("Error fetching words:", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Chinese HSK Words</h1>
+        <Tabs levels={levels} currentLevel={currentLevel} onSelectLevel={setCurrentLevel} />
+        <Search onSearch={setSearchQuery}/>
+      </div>
+      <WordList words={words} />
     </div>
   );
 }
